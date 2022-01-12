74cf23cd7243d123f3a260844e6996cbf21384c2
Alex Calatayud <4lexxe3@gmail.com>
2022-01-12 18:37:59 -0300

    terminado

---
+++ main.js
@@ -0,0 +1,38 @@
+const cosasQueAprendimos = [
+  {
+    tema: "terminal",
+    class: "",
+  },
+  {
+    tema: "node",
+    class: "",
+  },
+  {
+    tema: "oop",
+    class: "",
+  },
+  {
+    tema: "flexbox",
+    class: "",
+  },
+  {
+    tema: "css",
+    class: "",
+  },
+  {
+    tema: "dom",
+    class: "special",
+  },
+];
+
+function main() {
+  //codigo
+  cosasQueAprendimos.forEach((cosa) => {
+    const nuevoItem = document.createElement("li");
+    nuevoItem.innerText = cosa.tema;
+    cosa.class && nuevoItem.classList.add(cosa.class);
+    document.querySelector(".lista").appendChild(nuevoItem);
+  });
+}
+
+main();
